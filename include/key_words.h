4389110a6c2138c843a0470641ddaca7c3b64235
doushe821 <romachevskii.ae@phystech.edu>
2025-05-13 02:52:01 +0300

    Removed very last traces of prefixes

--- include/key_words.h
+++ include/key_words.h
@@ -33,16 +33,6 @@ const char kIR_KEY_WORD_ARRAY[kIR_KEY_WORD_NUMBER][kIR_KEY_WORD_NAME_MAX] =
 
 const size_t kIR_OPERAND_IDENTIFIERS_NUMBER   = 3;
 const size_t kIR_OPERAND_IDENTIFIERS_NAME_MAX = 16;
- 
-// I made both index and code enum in case we will add some non-numerical 
-// prefixes to anything, string array will probably be deleted in the future
-
-enum IR_Operands_Identifiers_Indexes
-{
-    IR_TMP_OPERAND_INDEX,
-    IR_VAR_OPERAND_INDEX,
-    IR_ARG_OPERAND_INDEX,
-};
 
 const size_t kIR_SYS_CALL_NUMBER   = 3;
 const size_t kIR_SYS_CALL_NAME_MAX = 16;
